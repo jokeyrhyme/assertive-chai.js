bc5017a141c24a02a2600208069f31d8bbbce676
Ron Waldon <jokeyrhyme@gmail.com>
2014-05-23 16:43:52 +1000

    automated tests now pass 49 out of 49 !

--- assertive-chai.js
+++ assertive-chai.js
@@ -18,6 +18,60 @@
 
   var chai, natives, typeOf;
 
+  // Array.prototype.indexOf poly-fill
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+      var length;
+      if ( this === undefined || this === null ) {
+        throw new TypeError( '"this" is null or not defined' );
+      }
+      length = this.length >>> 0; // Hack to convert object.length to a UInt32
+      fromIndex = +fromIndex || 0;
+      if (Math.abs(fromIndex) === Infinity) {
+        fromIndex = 0;
+      }
+      if (fromIndex < 0) {
+        fromIndex += length;
+        if (fromIndex < 0) {
+          fromIndex = 0;
+        }
+      }
+      for (;fromIndex < length; fromIndex++) {
+        if (this[fromIndex] === searchElement) {
+          return fromIndex;
+        }
+      }
+      return -1;
+    };
+  }
+
+  // Array.prototype.map poly-fill
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+  if (!Array.prototype.map) {
+    Array.prototype.map = function(fun /*, thisArg */) {
+      "use strict";
+      if (this === void 0 || this === null) {
+        throw new TypeError();
+      }
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== "function") {
+        throw new TypeError();
+      }
+      var res = new Array(len);
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++) {
+        if (i in t) {
+          res[i] = fun.call(thisArg, t[i], i, t);
+        }
+      }
+      return res;
+    };
+  }
+
   chai = {};
 
   // import Node.JS's assertions
@@ -26,11 +80,10 @@
     return assert.apply(this, arguments);
   };
 
-  chai.assert.deepEqual = assert.deepEqual;
   chai.assert.equal = assert.equal;
   chai.assert.fail = assert.fail;
   chai.assert.strictEqual = assert.strictEqual;
-  chai.assert.throws = assert.throws;
+  chai.assert['throws'] = assert['throws'];
 
   chai.assert.notDeepEqual = assert.notDeepEqual;
   chai.assert.notEqual = assert.notEqual;
@@ -61,7 +114,74 @@
     return typeof obj;
   };
 
+  // https://github.com/chaijs/chai/blob/master/lib/chai/utils/getPathValue.js
+
+  function _getPathValue (parsed, obj) {
+    var tmp = obj
+      , res;
+    for (var i = 0, l = parsed.length; i < l; i++) {
+      var part = parsed[i];
+      if (tmp) {
+        if ('undefined' !== typeof part.p)
+          tmp = tmp[part.p];
+        else if ('undefined' !== typeof part.i)
+          tmp = tmp[part.i];
+        if (i == (l - 1)) res = tmp;
+      } else {
+        res = undefined;
+      }
+    }
+    return res;
+  };
+
+  function parsePath (path) {
+    var str = path.replace(/\[/g, '.[')
+      , parts = str.match(/(\\\.|[^.]+?)+/g);
+    return parts.map(function (value) {
+      var re = /\[(\d+)\]$/
+        , mArr = re.exec(value)
+      if (mArr) return { i: parseFloat(mArr[1], 10) };
+      else return { p: value };
+    });
+  };
+
+  function getPathValue (path, obj) {
+    var parsed = parsePath(path);
+    return _getPathValue(parsed, obj);
+  };
+
   // implement Chai.JS's assertions
+
+  chai.assert.deepEqual = function (actual, expected, msg) {
+    if (typeOf(actual) === 'regexp') {
+      actual = actual.toString();
+    }
+    if (typeOf(actual) === 'date') {
+      actual = actual.getTime();
+    }
+    if (typeOf(expected) === 'regexp') {
+      expected = expected.toString();
+    }
+    if (typeOf(expected) === 'date') {
+      expected = expected.getTime();
+    }
+    return assert.equal(typeOf(actual), typeOf(expected), msg) &&
+      assert.deepEqual(actual, expected, msg);
+  };
+
+  chai.assert.notDeepEqual = function (actual, expected, msg) {
+    if (typeOf(actual) === 'regexp' || typeOf(actual) === 'date') {
+      actual = actual.toString();
+    }
+    if (typeOf(expected) === 'regexp' || typeOf(expected) === 'date') {
+      expected = expected.toString();
+    }
+    try {
+      return assert.notEqual(typeOf(actual), typeOf(expected), msg);
+    } catch (e) {
+      return assert.notDeepEqual(actual, expected, msg);
+    }
+  };
 
   chai.assert.isTrue = function (value, msg) {
     return chai.assert.equal(value, true, msg);
@@ -163,6 +283,162 @@
 
   chai.assert.isNotBoolean = function (value, msg) {
     return chai.assert.notTypeOf(value, 'boolean', msg);
+  };
+
+  chai.assert.include = function (haystack, needle, msg) {
+    var prop;
+    if (typeOf(haystack) === 'object' && typeOf(needle) === 'object') {
+      for (prop in needle) {
+        if (needle.hasOwnProperty(prop) && haystack.hasOwnProperty(prop)) {
+          chai.assert.equal(haystack[prop], needle[prop], msg);
+        }
+      }
+      return;
+    }
+    return chai.assert.isFunction(haystack.indexOf) &&
+      chai.assert.notEqual(haystack.indexOf(needle), -1, msg);
+  };
+
+  chai.assert.notInclude = function (haystack, needle, msg) {
+    var prop;
+    if (typeOf(haystack) === 'object' && typeOf(needle) === 'object') {
+      for (prop in needle) {
+        if (needle.hasOwnProperty(prop) && haystack.hasOwnProperty(prop)) {
+          chai.assert.notEqual(haystack[prop], needle[prop], msg);
+        }
+      }
+      return;
+    }
+    if (typeOf(haystack) === 'array' && typeOf(haystack) === 'string') {
+      return chai.assert.equal(haystack.indexOf(needle), -1, msg);
+    }
+  };
+
+  chai.assert.lengthOf = function (obj, length, msg) {
+    if (obj && typeOf(obj.length) === 'number') {
+      return chai.assert.equal(obj.length, length, msg);
+    }
+  };
+
+  chai.assert.match = function (value, regexp, msg) {
+    return chai.assert.instanceOf(regexp, RegExp) &&
+      chai.assert.isTrue(regexp.test(value), msg);
+  };
+
+  chai.assert.notMatch = function (value, regexp, msg) {
+    return chai.assert.instanceOf(regexp, RegExp) &&
+      chai.assert.isFalse(regexp.test(value), msg);
+  };
+
+  chai.assert.property = function (object, property, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    return chai.assert.isDefined(object[property], msg);
+  };
+
+  chai.assert.notProperty = function (object, property, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    return chai.assert.isUndefined(object[property], msg);
+  };
+
+  chai.assert.deepProperty = function (object, property, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    return chai.assert.isDefined(getPathValue(property, object), msg);
+  };
+
+  chai.assert.notDeepProperty = function (object, property, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    return chai.assert.isUndefined(getPathValue(property, object), msg);
+  };
+
+  chai.assert.propertyVal = function (object, property, value, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    chai.assert.isDefined(object[property], msg);
+    return chai.assert.equal(object[property], value, msg);
+  };
+
+  chai.assert.notPropertyVal = function (object, property, value, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    if (typeOf(object[property]) === 'undefined') {
+      return chai.assert.notProperty(object, property, msg);
+    }
+    return chai.assert.notEqual(object[property], value, msg);
+  };
+
+  chai.assert.deepPropertyVal = function (object, property, value, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    return chai.assert.equal(getPathValue(property, object), value, msg);
+  };
+
+  chai.assert.deepPropertyNotVal = function (object, property, value, msg) {
+    chai.assert.instanceOf(object, Object);
+    chai.assert.isString(property);
+    return chai.assert.notEqual(getPathValue(property, object), value, msg);
+  };
+
+  chai.assert.operator = function (val1, operator, val2, msg) {
+    chai.assert.isString(operator);
+    switch (operator) {
+      case '<':
+        return chai.assert.isTrue(val1 < val2, msg);
+        break;
+      case '<=':
+        return chai.assert.isTrue(val1 <= val2, msg);
+        break;
+      case '>':
+        return chai.assert.isTrue(val1 > val2, msg);
+        break;
+      case '>=':
+        return chai.assert.isTrue(val1 >= val2, msg);
+        break;
+      case '==':
+        return chai.assert.isTrue(val1 == val2, msg);
+        break;
+      case '!=':
+        return chai.assert.isTrue(val1 != val2, msg);
+        break;
+      case '===':
+        return chai.assert.isTrue(val1 === val2, msg);
+        break;
+      case '!==':
+        return chai.assert.isTrue(val1 !== val2, msg);
+        break;
+      default:
+        throw new Error('unsupported operator');
+    }
+  };
+
+  chai.assert.closeTo = function (actual, expected, delta, msg) {
+    chai.assert.isNumber(actual);
+    chai.assert.isNumber(expected);
+    chai.assert.isNumber(delta);
+    return chai.assert.isTrue(Math.abs(actual - expected) <= delta, msg);
+  };
+
+  chai.assert.sameMembers = function (set1, set2, msg) {
+    var length;
+    chai.assert.isArray(set1, msg);
+    chai.assert.isArray(set2, msg);
+    length = set1.length;
+    chai.assert.equal(length, set2.length, msg);
+    return chai.assert.includeMembers(set1, set2, msg);
+  };
+
+  chai.assert.includeMembers = function (superset, subset, msg) {
+    var length;
+    chai.assert.isArray(superset, msg);
+    chai.assert.isArray(subset, msg);
+    length = subset.length;
+    while (length > 0) {
+      length -= 1;
+      chai.assert.include(superset, subset[length], msg);
+    }
   };
 
   return chai;
